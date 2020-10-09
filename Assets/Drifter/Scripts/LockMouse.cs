46b7233f7b187a7febc249a8120ce611ea8496b8
Ty Cobb <ty.cobb.m@gmail.com>
2020-10-08 21:34:58 -0400

    add required assets

---
+++ Assets/Drifter/Scripts/LockMouse.cs
@@ -0,0 +1,30 @@
+﻿// by @torahhorse
+
+using UnityEngine;
+using System.Collections;
+
+public class LockMouse : MonoBehaviour {
+  void Start() {
+    LockCursor(true);
+  }
+
+  void Update() {
+    // lock when mouse is clicked
+    if( Input.GetMouseButtonDown(0) && Time.timeScale > 0.0f ) {
+      LockCursor(true);
+    }
+
+    // unlock when escape is hit
+    if (Input.GetKeyDown(KeyCode.Escape)) {
+      LockCursor(!IsCursorLocked());
+    }
+  }
+
+  public void LockCursor(bool lockCursor) {
+    Cursor.lockState = lockCursor ? CursorLockMode.Locked : CursorLockMode.None;
+  }
+
+  public bool IsCursorLocked() {
+    return Cursor.lockState == CursorLockMode.Locked;
+  }
+}
