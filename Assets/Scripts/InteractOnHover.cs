4a267ba94b94d5422874cbf81f99c34b88f9d532
Ty Cobb <ty.cobb.m@gmail.com>
2020-10-12 22:52:20 -0400

    add fade to interact prompt

--- Assets/Scripts/InteractOnHover.cs
+++ Assets/Scripts/InteractOnHover.cs
@@ -1,35 +1,104 @@
-﻿using System;
+﻿using System.Collections;
 using UnityEngine;
 using TMPro;
 
 public class InteractOnHover: MonoBehaviour {
+    // -- constants --
+    private const float kDuration = 0.2f;
+
     // -- fields --
     [SerializeField]
     [Tooltip("The text to display in the prompt.")]
-    private string f_Prompt = "";
+    private string fPrompt = "";
 
     [SerializeField]
     [Tooltip("The minimum distance before showing the tooltip.")]
-    private float f_MinDistance = 20.0f;
+    private float fMinDistance = 20.0f;
 
     // -- props --
-    private GameObject m_Prompt;
+    private GameObject mPrompt;
+    private bool mIsVisible = false;
+    private IEnumerator mAnimation;
 
     // -- lifecycle --
     void Start() {
-        m_Prompt = Instantiate(LoadPrefab(), transform);
-        m_Prompt.GetComponent<TextMeshPro>().text = f_Prompt;
+        mPrompt = Instantiate(LoadPrefab(), transform);
+        mPrompt.GetComponent<TextMeshPro>().text = fPrompt;
     }
 
     void Update() {
         var isVisible = IsInView();
 
-        // only show the prompt when in view
-        m_Prompt.SetActive(isVisible);
-
         // if visible, make sure the prompt faces the camera
         if (isVisible) {
-            m_Prompt.transform.forward = MainCamera().transform.forward;
+            mPrompt.transform.forward = MainCamera().transform.forward;
+        }
+
+        // if visibility changed, update prompt
+        if (isVisible != mIsVisible) {
+            mIsVisible = isVisible;
+            StartCoroutine(ShowPrompt(isVisible));
+        }
+    }
+
+    // -- commands --
+    private IEnumerator ShowPrompt(bool isVisible) {
+        if (isVisible) {
+            mPrompt.SetActive(true);
+            yield return Animate(FadePrompt(from: 0.0f, to: 1.0f));
+        } else {
+            yield return Animate(FadePrompt(from: 1.0f, to: 0.0f));
+            mPrompt.SetActive(false);
+        }
+    }
+
+    // -- animations --
+    private IEnumerator Animate(IEnumerator animation) {
+        if (mAnimation != null) {
+            StopCoroutine(mAnimation);
+        }
+
+        mAnimation = animation;
+        yield return animation;
+        mAnimation = null;
+    }
+
+    private IEnumerator FadePrompt(float from, float to) {
+        // set initial alpha
+        SetPromptAlpha(from);
+
+        // capture delta
+        var delta = to - from;
+
+        // calculate end time
+        var now = Time.time;
+        var stop = now + kDuration;
+
+        // animate every frame until the duration elapses
+        while (now < stop) {
+            yield return 0;
+
+            now = Time.time;
+            var percent = 1 - (stop - now) / kDuration;
+            SetPromptAlpha(from + Mathf.Min(percent, 1.0f) * delta);
+        }
+    }
+
+    private void SetPromptAlpha(float alpha) {
+        var renderers = mPrompt.GetComponentsInChildren<MeshRenderer>();
+
+        foreach (var renderer in renderers) {
+            var label = renderer.GetComponent<TextMeshPro>();
+
+            // set color on the text mesh if available
+            if (label != null) {
+                label.color = label.color.WithAlpha(alpha);
+            }
+            // otherwise try the material
+            else {
+                var material = renderer.material;
+                material.color = material.color.WithAlpha(alpha);
+            }
         }
     }
 
@@ -51,7 +120,7 @@ public class InteractOnHover: MonoBehaviour {
         // check if the a raycast hits this object
         RaycastHit contact;
         var source = camera.transform;
-        var didHit = Physics.Raycast(source.position, source.forward, out contact, f_MinDistance);
+        var didHit = Physics.Raycast(source.position, source.forward, out contact, fMinDistance);
 
         if (!didHit || contact.transform != transform) {
             return false;
@@ -60,8 +129,13 @@ public class InteractOnHover: MonoBehaviour {
         return true;
     }
 
+    // -- accessors --
     private Camera MainCamera() {
         return Camera.main;
+    }
+
+    private TextMeshPro TextMesh() {
+        return GetComponent<TextMeshPro>();
     }
 
     // -- factories --
