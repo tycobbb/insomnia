ed504752b61f528b6a0eab9df689f968c578682e
Ty Cobb <ty.cobb.m@gmail.com>
2020-10-16 12:31:28 -0400

    hack to fix camera jump on start

--- Assets/Drifter/Scripts/MouseLook.cs
+++ Assets/Drifter/Scripts/MouseLook.cs
@@ -12,114 +12,132 @@ using System.Collections.Generic;
 
 public class MouseLook : MonoBehaviour
 {
- 
-	public enum RotationAxes { MouseX = 1, MouseY = 2 }
-	public RotationAxes axes = RotationAxes.MouseX;
-	public bool invertY = false;
-	
-	public float sensitivityX = 10F;
-	public float sensitivityY = 9F;
- 
-	public float minimumX = -360F;
-	public float maximumX = 360F;
- 
-	public float minimumY = -85F;
-	public float maximumY = 85F;
- 
-	float rotationX = 0F;
-	float rotationY = 0F;
- 
-	private List<float> rotArrayX = new List<float>();
-	float rotAverageX = 0F;	
- 
-	private List<float> rotArrayY = new List<float>();
-	float rotAverageY = 0F;
- 
-	public float framesOfSmoothing = 5;
- 
-	Quaternion originalRotation;
-	
-	void Start ()
-	{			
-		if (GetComponent<Rigidbody>())
-		{
-			GetComponent<Rigidbody>().freezeRotation = true;
-		}
-		
-		originalRotation = transform.localRotation;
-	}
- 
-	void Update ()
-	{
-		if (axes == RotationAxes.MouseX)
-		{			
-			rotAverageX = 0f;
- 
-			rotationX += Input.GetAxis("Mouse X") * sensitivityX * Time.timeScale;
- 
-			rotArrayX.Add(rotationX);
- 
-			if (rotArrayX.Count >= framesOfSmoothing)
-			{
-				rotArrayX.RemoveAt(0);
-			}
-			for(int i = 0; i < rotArrayX.Count; i++)
-			{
-				rotAverageX += rotArrayX[i];
-			}
-			rotAverageX /= rotArrayX.Count;
-			rotAverageX = ClampAngle(rotAverageX, minimumX, maximumX);
- 
-			Quaternion xQuaternion = Quaternion.AngleAxis (rotAverageX, Vector3.up);
-			transform.localRotation = originalRotation * xQuaternion;			
-		}
-		else
-		{			
-			rotAverageY = 0f;
- 
- 			float invertFlag = 1f;
- 			if( invertY )
- 			{
- 				invertFlag = -1f;
- 			}
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY * invertFlag * Time.timeScale;
-			
-			rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
- 	
-			rotArrayY.Add(rotationY);
- 
-			if (rotArrayY.Count >= framesOfSmoothing)
-			{
-				rotArrayY.RemoveAt(0);
-			}
-			for(int j = 0; j < rotArrayY.Count; j++)
-			{
-				rotAverageY += rotArrayY[j];
-			}
-			rotAverageY /= rotArrayY.Count;
- 
-			Quaternion yQuaternion = Quaternion.AngleAxis (rotAverageY, Vector3.left);
-			transform.localRotation = originalRotation * yQuaternion;
-		}
-	}
-	
-	public void SetSensitivity(float s)
-	{
-		sensitivityX = s;
-		sensitivityY = s;
-	}
- 
-	public static float ClampAngle (float angle, float min, float max)
-	{
-		angle = angle % 360;
-		if ((angle >= -360F) && (angle <= 360F)) {
-			if (angle < -360F) {
-				angle += 360F;
-			}
-			if (angle > 360F) {
-				angle -= 360F;
-			}			
-		}
-		return Mathf.Clamp (angle, min, max);
-	}
+
+    public enum RotationAxes { MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseX;
+    public bool invertY = false;
+
+    public float sensitivityX = 10F;
+    public float sensitivityY = 9F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -85F;
+    public float maximumY = 85F;
+
+    float rotationX = 0F;
+    float rotationY = 0F;
+
+    private List<float> rotArrayX = new List<float>();
+    float rotAverageX = 0F;
+
+    private List<float> rotArrayY = new List<float>();
+    float rotAverageY = 0F;
+
+    public float framesOfSmoothing = 5;
+
+    Quaternion originalRotation;
+
+    // !!!: Our custom hacks.
+    private const int kFramesToSkip = 1;
+    private int mSkipFrameX = 0;
+    private int mSkipFrameY = 0;
+
+    void Start ()
+    {
+        if (GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }
+
+        originalRotation = transform.localRotation;
+    }
+
+    void Update ()
+    {
+        if (axes == RotationAxes.MouseX)
+        {
+            rotAverageX = 0f;
+
+            var mouse = Input.GetAxis("Mouse X");
+            if (mouse != 0.0f && mSkipFrameX < kFramesToSkip) {
+                mSkipFrameX++;
+                return;
+            }
+
+            rotationX += mouse * sensitivityX * Time.timeScale;
+
+            rotArrayX.Add(rotationX);
+
+            if (rotArrayX.Count >= framesOfSmoothing)
+            {
+                rotArrayX.RemoveAt(0);
+            }
+            for(int i = 0; i < rotArrayX.Count; i++)
+            {
+                rotAverageX += rotArrayX[i];
+            }
+            rotAverageX /= rotArrayX.Count;
+            rotAverageX = ClampAngle(rotAverageX, minimumX, maximumX);
+
+            Quaternion xQuaternion = Quaternion.AngleAxis (rotAverageX, Vector3.up);
+            transform.localRotation = originalRotation * xQuaternion;
+        }
+        else
+        {
+            rotAverageY = 0f;
+
+             float invertFlag = 1f;
+             if( invertY )
+             {
+                 invertFlag = -1f;
+             }
+
+            var mouse = Input.GetAxis("Mouse Y");
+            if (mouse != 0.0f && mSkipFrameY < kFramesToSkip) {
+                mSkipFrameY++;
+                return;
+            }
+
+            rotationY += mouse * sensitivityY * invertFlag * Time.timeScale;
+
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            rotArrayY.Add(rotationY);
+
+            if (rotArrayY.Count >= framesOfSmoothing)
+            {
+                rotArrayY.RemoveAt(0);
+            }
+            for(int j = 0; j < rotArrayY.Count; j++)
+            {
+                rotAverageY += rotArrayY[j];
+            }
+            rotAverageY /= rotArrayY.Count;
+
+            Quaternion yQuaternion = Quaternion.AngleAxis (rotAverageY, Vector3.left);
+            transform.localRotation = originalRotation * yQuaternion;
+        }
+    }
+
+    public void SetSensitivity(float s)
+    {
+        sensitivityX = s;
+        sensitivityY = s;
+    }
+
+    public static float ClampAngle (float angle, float min, float max)
+    {
+        angle = angle % 360;
+        if ((angle >= -360F) && (angle <= 360F)) {
+            if (angle < -360F) {
+                angle += 360F;
+            }
+            if (angle > 360F) {
+                angle -= 360F;
+            }
+        }
+        return Mathf.Clamp (angle, min, max);
+    }
 }
