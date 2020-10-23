26ae199a6fe589989eeb8d49df23acd25bc3b0c2
Ina Joe <ij2049@nyu.edu>
2020-10-24 02:41:50 +0900

    Revising sheep room

---
+++ Assets/Scripts/SheepRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    protected void Update()
+    {
+        var camera = Camera.main;
+        if (Vector3.Distance(transform.position, camera.transform.position) < 10.0f)
+        {
+            transform.forward = camera.transform.forward * -1;
+        }
+    }
+}
