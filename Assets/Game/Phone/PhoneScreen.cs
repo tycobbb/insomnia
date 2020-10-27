b3ce490c8cf95d76b57188f60856a4d5dc522ea8
Ty Cobb <ty.cobb.m@gmail.com>
2020-10-26 21:20:24 -0400

    add persistent phone sounds

--- Assets/Game/Phone/PhoneScreen.cs
+++ Assets/Game/Phone/PhoneScreen.cs
@@ -1,20 +1,45 @@
+using System;
 using UnityEngine;
 using TMPro;
 
 public class PhoneScreen: MonoBehaviour {
     // -- fields --
     [SerializeField]
-    [Tooltip("The time label.")]
+    [Tooltip("If the screen is on by default.")]
+    private bool fIsOn = false;
+
+    [SerializeField]
+    [Tooltip("The time field.")]
     private TextMeshPro fTime = null;
 
     [SerializeField]
     [Tooltip("The material when the screen is on.")]
     private Material fScreenOn = null;
 
+    [SerializeField] [Tooltip("The sounds to play when the screen is on.")]
+    private AudioClip[] fSounds = null;
+
+    // -- props --
+    private Renderer mRenderer;
+    private AmbientSound mAmbientSound;
+
+    // -- lifecycle --
+    protected void Awake() {
+        mRenderer = GetComponent<Renderer>();
+        mAmbientSound = GetComponent<AmbientSound>();
+    }
+
+    protected void Start() {
+        if (fIsOn) {
+            TurnOn();
+        }
+    }
+
     // -- commands --
     public void TurnOn() {
-        GetComponent<Renderer>().material = fScreenOn;
         fTime.gameObject.SetActive(true);
+        mRenderer.material = fScreenOn;
+        mAmbientSound.Play(fSounds);
     }
 
     public void SetTime(string time) {
